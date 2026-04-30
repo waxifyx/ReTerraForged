9ab29d59c1f43b03c8c590d75ccd7e1172771b58
Waxify <147958798+waxifyx@users.noreply.github.com>
2026-04-30 22:37:08 +0900

    Add Kilt compatibility patches

--- common/src/main/java/raccoonman/reterraforged/registries/RTFRegistries.java
+++ common/src/main/java/raccoonman/reterraforged/registries/RTFRegistries.java
@@ -22,15 +22,15 @@ public class RTFRegistries {
 	public static final ResourceKey<Registry<Codec<? extends ChanceModifier>>> CHANCE_MODIFIER_TYPE = createKey("worldgen/chance_modifier_type");
 	public static final ResourceKey<Registry<Codec<? extends TemplatePlacement<?>>>> TEMPLATE_PLACEMENT_TYPE = createKey("worldgen/template_placement_type");
 	public static final ResourceKey<Registry<Codec<? extends TemplateDecorator<?>>>> TEMPLATE_DECORATOR_TYPE = createKey("worldgen/template_decorator_type");
-	public static final ResourceKey<Registry<Codec<? extends BiomeModifier>>> BIOME_MODIFIER_TYPE = createKey("forge:biome_modifier_serializers");
+	public static final ResourceKey<Registry<Codec<? extends BiomeModifier>>> BIOME_MODIFIER_TYPE = createKey("worldgen/biome_modifier_type");
 	public static final ResourceKey<Registry<Codec<? extends StructureRule>>> STRUCTURE_RULE_TYPE = createKey("worldgen/structure_rule_type");
 	public static final ResourceKey<Registry<Noise>> NOISE = createKey("worldgen/noise");
-	public static final ResourceKey<Registry<BiomeModifier>> BIOME_MODIFIER = createKey("forge:biome_modifier");
+	public static final ResourceKey<Registry<BiomeModifier>> BIOME_MODIFIER = createKey("worldgen/biome_modifier");
 	public static final ResourceKey<Registry<StructureRule>> STRUCTURE_RULE = createKey("worldgen/structure_rule");
 
 	@Deprecated
 	public static final ResourceKey<Registry<Preset>> PRESET = createKey("worldgen/preset");
-	
+
 	public static <T> ResourceKey<T> createKey(ResourceKey<? extends Registry<T>> registryKey, String valueKey) {
 		return ResourceKey.create(registryKey, RTFCommon.location(valueKey));
 	}
--- fabric/src/main/java/raccoonman/reterraforged/world/worldgen/biome/modifier/fabric/BiomeModifiersImpl.java
+++ fabric/src/main/java/raccoonman/reterraforged/world/worldgen/biome/modifier/fabric/BiomeModifiersImpl.java
@@ -19,20 +19,12 @@ import raccoonman.reterraforged.world.worldgen.biome.modifier.Filter;
 import raccoonman.reterraforged.world.worldgen.biome.modifier.Order;
 
 public class BiomeModifiersImpl {
-	
+
 	public static void bootstrap() {
 		register("add", AddModifier.CODEC);
 		register("replace", ReplaceModifier.CODEC);
-		
-		//prevent forge biome modifiers from being loaded
-		//FIXME this is a bad way to do this 
-		register("forge:none", Dummy.makeCodec());
-		register("forge:add_features", Dummy.makeCodec());
-		register("forge:remove_features", Dummy.makeCodec());
-		register("forge:add_spawns", Dummy.makeCodec());
-		register("forge:remove_spawns", Dummy.makeCodec());
 	}
-	
+
 	public static BiomeModifier add(Order order, GenerationStep.Decoration step, Optional<Pair<Filter.Behavior, HolderSet<Biome>>> biomes, HolderSet<PlacedFeature> features) {
 		return new AddModifier(order, step, biomes.map((p) -> new Filter(p.getSecond(), p.getFirst())), features);
 	}
@@ -40,20 +32,8 @@ public class BiomeModifiersImpl {
 	public static BiomeModifier replace(GenerationStep.Decoration step, Optional<HolderSet<Biome>> biomes, Map<ResourceKey<PlacedFeature>, Holder<PlacedFeature>> replacements) {
 		return new ReplaceModifier(step, biomes, replacements);
 	}
-	
+
 	public static void register(String name, Codec<? extends BiomeModifier> value) {
 		RegistryUtil.register(RTFBuiltInRegistries.BIOME_MODIFIER_TYPE, name, value);
 	}
-
-	private record Dummy() implements BiomeModifier	{
-		
-		@Override
-		public Codec<Dummy> codec() {
-			return makeCodec();
-		}
-		
-		public static Codec<Dummy> makeCodec() {
-			return Codec.unit(Dummy::new);
-		}
-	};
 }
